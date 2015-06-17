734e96807e06187f743c0e5cd2b77ed447b464a6
Justin Kim <jkim430@gmail.com>
2015-06-17 14:32:47 -0400

    added start script

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var app = express();
 app.use(morgan('dev'));
 
 app.get('/', function(req,res) {
-	res.send('Hello world');
+	res.send('Hello world!!!!!');
 });
 
-
+app.get('/news', function(req,res) {
+	res.send('No news');
+});
 
 
 
